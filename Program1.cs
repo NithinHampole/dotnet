1522de0a6cf51ba98a32b95b8eb8e0302a450cd5
NithinHampole <nithinhampole123@gmail.com>
2025-02-11 17:50:49 +0530

    program 1 added to repo

---
+++ Program1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ProgramList
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Input Text");
+            StringBuilder SavedInput = new StringBuilder();
+            bool flag = false;
+            try
+            {
+
+
+                while (flag != true)
+                {
+                    string input = Console.ReadLine();
+                    if (input.Contains("STOP"))
+                    {
+                        flag = true;
+                        break;
+                    }
+                    SavedInput.AppendLine(input);
+                }
+
+                string Path = @"C:\Users\Prodigy\Desktop\Read\output.txt";
+                File.WriteAllText(Path, SavedInput.ToString());
+
+                string Content = File.ReadAllText(Path);
+                Console.WriteLine("\nReading Content of file...");
+                Console.WriteLine();
+                Console.WriteLine(Content);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine($"Error:{e.Message} You do not have permission to access the file ");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"Error:{e.Message} Check your Directory");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error:{e.Message}");
+            }
+
+        }
+    }
+
+}
+
