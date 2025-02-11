74b95e98908104a6545a3b4443993140d027491a
NithinHampole <nithinhampole123@gmail.com>
2025-02-11 15:22:22 +0530

    program4

---
+++ Program4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProgramList
+{
+    class Program4
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter your Order Name:");
+            string orderName = Console.ReadLine();
+
+            await ProcessOrderAsync(orderName);
+        }
+
+        static async Task ProcessOrderAsync(string item)
+        {
+            Console.WriteLine("Processing Order...........");
+            await Task.Delay(3000);
+            Console.WriteLine($"Order for {item} is ready!");
+        }
+    }
+}
