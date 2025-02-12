270c17a17b3416ae9efb3463b483786e1fe11890
NithinHampole <nithinhampole123@gmail.com>
2025-02-12 16:26:59 +0530

    added 5 programs

---
+++ Program5A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class SingleThreadedWriter
+{
+    static void Main()
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        string directoryPath = "SingleThreadedFiles";
+        Directory.CreateDirectory(directoryPath);
+
+        byte[] dataBlock = new byte[50 * 1024]; // 50kB block
+        new Random().NextBytes(dataBlock); // Fill block with random data
+
+        for (int i = 1; i <= 100; i++)
+        {
+            string fileName = Path.Combine(directoryPath, $"File_{i}.bin");
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                for (int j = 0; j < 200; j++) // 200 * 50kB = 10MB
+                {
+                    fs.Write(dataBlock, 0, dataBlock.Length);
+                }
+            }
+            Console.WriteLine($"{fileName} Writing Completed");
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Single-threaded execution time: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
---
+++ Program5B.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+class MultiThreadedWriter
+{
+    static async Task Main()
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        string directoryPath = "MultiThread";
+        Directory.CreateDirectory(directoryPath);
+
+        byte[] dataBlock = new byte[50 * 1024];
+        new Random().NextBytes(dataBlock);
+
+        Task[] tasks = new Task[100];
+
+        for (int i = 1; i <= 100; i++)
+        {
+            int fileNumber = i;
+            tasks[i - 1] = Task.Run(async () =>
+            {
+                string fileName = Path.Combine(directoryPath, $"File_{fileNumber}.bin");
+                using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None, 4096, useAsync: true))
+                {
+                    for (int j = 0; j < 200; j++)
+                    {
+                        await fs.WriteAsync(dataBlock, 0, dataBlock.Length);
+                    }
+                }
+                Console.WriteLine($"{fileName} Writing Completed");
+            });
+        }
+
+        await Task.WhenAll(tasks);
+
+        stopwatch.Stop();
+        Console.WriteLine($"Multi-threaded execution time: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
